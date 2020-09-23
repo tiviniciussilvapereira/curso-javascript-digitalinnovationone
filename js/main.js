a4051e03c0128843d8533c44469564476a1f89f7
Vinicius Silva Pereira <ti.viniciussilvapereira@gmail.com>
2020-09-23 14:43:41 -0300

    introducao

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//var nome = "Vinicius";
+//var idade  = 26; 
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade s+ idade2);
+//console.log(nome); 
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil")); 
+//console.log(frase.toUpperCase());
+//alert(frase.replace("Japão", "Brasil"))
